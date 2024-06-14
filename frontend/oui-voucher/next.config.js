c6c0d98be7b8ca778981965a0e6a17caea1097cd
Xpiral397 <111965825+Xpiral397@users.noreply.github.com>
2024-06-14 07:57:52 +0100

    phase -1

--- frontend/oui-voucher/next.config.js
+++ frontend/oui-voucher/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig={
-    ignoreBuildError:true,
+
     eslint: {
-        ignoreDruingBuild: true
+        ignoreDuringBuilds: true
     }
 }
 module.exports = nextConfig
