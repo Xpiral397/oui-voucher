784e3ceb3ec12c95662a1356b00cbb40b5f3db9d
Xpiral397 <111965825+Xpiral397@users.noreply.github.com>
2024-08-20 18:36:30 +0100

    all

--- frontend/oui-voucher/app/admin/dashboard/recharge/page.tsx
+++ frontend/oui-voucher/app/admin/dashboard/recharge/page.tsx
@@ -54,7 +54,6 @@ export function VoucherGenerator() {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authentication: `Token ${getToken("Admin")}`,
         },
         body: JSON.stringify(vouchers),
       });
@@ -233,7 +232,6 @@ export function VoucherGenerator() {
 
 import { useTheme } from "next-themes";
 import axios from "axios";
-import { getToken } from "@/app/controller/auth/auth";
 
 interface Voucher {
   amount: string;
